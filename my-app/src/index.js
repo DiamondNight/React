747cc3f7e847c8f11b91a68b209dfc7a1204229a
DiamondNight <luisfernando.castroalves2002@gmail.com>
2023-11-25 12:38:09 +0000

    Component Properties

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,10 @@
 import './index.css'
 import ReactDOM from 'react-dom/client';
+
+// Components
 import Primerio from './components/basic/Primeiro';
+import ComParametro from './components/basic/ComParametro';
+
 const tag = <strong>Hello React WORLD!</strong>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,6 +12,7 @@ root.render(
     {tag}
     <br></br>
     <Primerio />
+    <ComParametro titulo = "Classificações [React]" aluno = "Luis Alves" nota = {16.8} />
   </div>
 );
 
