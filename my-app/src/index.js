087d7d486253be69012638f5c920e9f2b2693c0a
DiamondNight <luisfernando.castroalves2002@gmail.com>
2023-11-25 12:28:38 +0000

    First Component - Part 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,13 @@
 import './index.css'
 import ReactDOM from 'react-dom/client';
+import Primerio from './components/basic/Primeiro';
 const tag = <strong>Hello React WORLD!</strong>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     {tag}
+    <br></br>
+    <Primerio />
   </div>
 );
 
