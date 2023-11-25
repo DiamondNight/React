feeda499ec57ba57516a1046cca9680c0d4b06b2
DiamondNight <luisfernando.castroalves2002@gmail.com>
2023-11-25 12:51:14 +0000

    Component Properties [Read Only]

--- my-app/src/components/basic/ComParametro.jsx
+++ my-app/src/components/basic/ComParametro.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function ComParametro(props) {
     console.log(props);
     const status = props.nota > 10 ? 'Aprobado' : 'Reprobado';
+    // props.nota = Math.ceil(props.nota) [No se puede modificar el valor del props]
+    let notaInt = Math.ceil(props.nota)
     return (
         <div>
             <h2>{props.titulo}</h2>
             <p>
-                Aluno - {props.aluno} - {props.nota} - {status}
+                Aluno - {props.aluno} - {notaInt} - {status}
             </p>
         </div>
     )
